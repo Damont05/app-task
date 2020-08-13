9193137df22defc7514a2ab7186370af915043e5
damon <luisdanielmonterofalcon@gmail.com>
2020-08-13 12:21:06 -0400

    modificando mongoClient

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const URI = 'mongodb://localhost/task';
 
-mongoose.connect(URI, { useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect(URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
     .then(db => console.log('----->Database is connected<-----'))
     .catch(err => console.error(err));
 
